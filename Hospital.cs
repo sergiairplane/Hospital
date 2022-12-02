8e37d1d39dacdee01a0fcea2e1f6480c1f2478f5
Sergi Andrés <sergi.andres@airplane.solutions>
2022-12-02 13:02:58 +0100

    Agregar archivos de proyecto.

---
+++ Hospital.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Hospital
+    {
+        List<Persona> personaList;
+        int habitaciones;
+
+        public Hospital(List<Persona> personaList, int habitaciones)
+        {
+            this.personaList = personaList;
+            this.habitaciones = habitaciones;
+        }
+
+
+
+        public void darMeditacion()
+        {
+
+        }
+
+
+        public void darAltaPaciente()
+        {
+
+        }
+
+    }
+}
---
+++ Paciente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Paciente : Persona
+    {
+        string diagnostico;
+        int diasIngresado;
+        string pronostico;
+        List<string> pruebasMedicas;
+
+        public Paciente(string diagnostico, int diasIngresado, string pronostico, List<string> pruebasMedicas)
+        {
+            this.diagnostico = diagnostico;
+            this.diasIngresado = diasIngresado;
+            this.pronostico = pronostico;
+            this.pruebasMedicas = pruebasMedicas;
+        }
+    }
+}
